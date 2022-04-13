90f583b8adca02a538ace5c6b4b3ac5a41079cb0
Tyler Sammons <tylersammons@Tylers-MacBook-Air.local>
2022-04-13 14:35:46 -0500

    update styling

--- js/script.js
+++ js/script.js
@@ -1,72 +1,69 @@
 "use strict";
+
 var ctx, canvas;
-var a, b;
+var PARAM_C = 1, 
+    PARAM_D = 1, 
+    SIZE = 2, 
+    T_INTERVAL = .0002;
 
 function init() {
+    window.addEventListener('resize', onWindowResize);
+    setupCanvas();
+    beginCurve();
+}
+
+function onWindowResize(event) {
+    setupCanvas()
+    reset();
+}
+
+function setupCanvas() {
     canvas = document.getElementById('myCanvas');
     ctx = myCanvas.getContext('2d');
-    canvas.width = window.innerWidth-100;
-    canvas.height = window.innerHeight;//-100;
+    canvas.width = window.innerWidth - 125;
+    canvas.height = window.innerHeight;
     ctx.lineWidth=10;
     ctx.strokeStyle="#FF0000";
-    ctx.fillStyle="black";
+    ctx.fillStyle="#00FF00";
 }
 
-function sinCurve(x) {
-    var i = x*10;
-    var y = Math.sin(x)*10;
-    y = y-400;
+function beginCurve() {
+    var t = Math.PI * 0;
 
-    for (var n = 0; n < 150; n++) {
-	ctx.fillRect((canvas.width/2+i),(canvas.height/2+y),1,1);
-	y = y+5;
-    }
-    x = x+0.1;
-    return x;
+    setInterval(() => {
+        t = plotLissajous(t);
+    }, 2);
 }
 
-function lissajous(x) {
-    var i;
-    var y;
-    
-    //var a = 12.25;
-    //var b = 11.25;
-    a = document.getElementById("a").value;
-    b = document.getElementById("b").value;
-    var c = 1;
-    var d = 1;
-    var speed =document.getElementById("speed").value;
-    var size = 2;
+function plotLissajous(t) {
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+    var speed = document.getElementById("speed").value;
+
+    var xAmplitude = canvas.width * 0.465;
+    var yAmplitude = canvas.height * 0.45;
 
     for (var n = 0; n < speed*100; n++) {
-	i = (canvas.width*0.5)*Math.sin(a*x+c);
-	y = (canvas.height*0.495)*Math.sin(b*x+d);
-	ctx.fillRect((canvas.width/2+i),(canvas.height/2+y),size,size);
-	x+=.0002;
+        var x = xAmplitude * Math.sin(t*a + PARAM_C);
+        var y = yAmplitude * Math.sin(t*b + PARAM_D);
+        drawPoint(x, y);
+        t += T_INTERVAL;
     }
 
+    return t;
+}
 
-    return x;
+function drawPoint(x, y) {
+    var xCenter = canvas.width * 0.5;
+    var yCenter = canvas.height * 0.5;
+    ctx.fillRect(xCenter + x, yCenter + y, SIZE, SIZE)
 }
 
 function reset() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    console.log(a);
-    console.log(b);
+    console.log(document.getElementById("a").value);
+    console.log(document.getElementById("b").value);
     console.log("~~~");
 }
 
-function go() {
-    var n = -22*Math.PI;
-    var end = setInterval(function() { n = sinCurve(n);}, 10);
-}
-
-function go2() {
-    var n = Math.PI/4;
-    var end = setInterval(function() { n = lissajous(n);}, 2);
-}
-
-
-
 init();
-go2();
